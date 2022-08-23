5d14c7572451a9b4e68a9a694bf8fc755316f4d0
Qiao Wang <wangqiao11@hotmail.com>
2022-08-23 23:37:43 +0800

    Support get state API

--- src/Dapr/HttpClient/StateManagement.hs
+++ src/Dapr/HttpClient/StateManagement.hs
@@ -4,16 +4,21 @@
 module Dapr.HttpClient.StateManagement where
 
 import Dapr.HttpClient.Core
-import Data.Aeson (KeyValue ((.=)), ToJSON (toJSON), object)
+import Data.Aeson (FromJSON, KeyValue ((.=)), ToJSON (toJSON), decode, object)
 import Network.HTTP.Req
 import RIO
+import RIO.Map (foldlWithKey)
 
 data ConsistencyLevel = Strong | Eventual
-  deriving (Eq, Show)
+  deriving (Eq)
 
 instance ToJSON ConsistencyLevel where
   toJSON Strong = "strong"
   toJSON Eventual = "eventual"
+
+instance Show ConsistencyLevel where
+  show Strong = "strong"
+  show Eventual = "eventual"
 
 data SaveStateOptions = SaveStateOptions
   { ssoConcurrency :: Text,
@@ -33,9 +38,9 @@ data SaveStateReqBody a = SaveStateReqBody
     ssrValue :: a,
     ssrEtag :: Maybe Text,
     ssrOptions :: Maybe SaveStateOptions,
-    ssrMetadata :: Maybe (HashMap Text Text)
+    ssrMetadata :: Maybe (Map Text Text)
   }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic)
 
 instance ToJSON a => ToJSON (SaveStateReqBody a) where
   toJSON (SaveStateReqBody k v etag opts meta) =
@@ -63,3 +68,21 @@ saveState config store body = do
 
 saveSingleState :: (MonadIO m, ToJSON a) => DaprClientConfig -> Text -> SaveStateReqBody a -> m (Either Text ())
 saveSingleState config store body = saveState config store [body]
+
+getState :: (MonadIO m, FromJSON a) => DaprClientConfig -> Text -> Text -> Maybe ConsistencyLevel -> Maybe (Map Text Text) -> m (Either Text a)
+getState config store key consistency metadata = do
+  let url = "state" <> "/" <> store <> "/" <> key
+      metadataQuery = maybe mempty (foldlWithKey (\query key' value -> query <> key' =: Just value) mempty) metadata
+      options = metadataQuery <> "consistency" =: (show <$> consistency)
+  response <- makeRequest config GET url NoReqBody lbsResponse options
+  case responseStatusCode response of
+    200 -> return $ case decode $ responseBody response of
+      Just x -> Right x
+      Nothing -> Left "Failed to decode response body"
+    204 -> return $ Left "Key is not found"
+    400 -> return $ Left "State store is missing or misconfigured"
+    500 -> return $ Left "Get state failed"
+    _ -> return $ Left "Unknown response status code"
+
+getStateSimple :: (MonadIO m, FromJSON a) => DaprClientConfig -> Text -> Text -> m (Either Text a)
+getStateSimple config store key = getState config store key Nothing Nothing
--- test/Dapr/HttpClientSpec.hs
+++ test/Dapr/HttpClientSpec.hs
@@ -23,3 +23,15 @@ spec = do
     it "Save state" $ do
       r <- saveState defaultDaprClientConfig "statestore" [mkSimleSaveStateReqBody "key" ("value" :: Text)]
       isRight r `shouldBe` True
+
+    it "Get state" $ do
+      r <- getState defaultDaprClientConfig "statestore" "key" Nothing Nothing
+      isRight r `shouldBe` True
+      let d = head $ rights [r]
+      d `shouldBe` ("value" :: Text)
+
+    it "Get state with false key" $ do
+      r <- getState defaultDaprClientConfig "statestore" "key1" Nothing Nothing :: IO (Either Text Text)
+      isRight r `shouldBe` False
+      let d = head $ lefts [r]
+      d `shouldBe` "Key is not found"
